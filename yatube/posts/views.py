6d72a645d724a2065e12dadd414075f8d46ab2fa
ash1k-dev <ash1kekb@yandex.ru>
2022-06-02 17:54:27 +0500

    2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 # Импортируем модель, чтобы обратиться к ней
 from .models import Post, Group
 
+
 def index(request):
-    # Одна строка вместо тысячи слов на SQL:
-    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
-    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
     posts = Post.objects.order_by('-pub_date')[:10]
     # В словаре context отправляем информацию в шаблон
     context = {
@@ -15,7 +12,7 @@ def index(request):
     return render(request, 'posts/index.html', context)
 
 
-def group_list(request, slug):
+def group_posts(request, slug):
     # Функция get_object_or_404 получает по заданным критериям объект
     # из базы данных или возвращает сообщение об ошибке, если объект не найден.
     # В нашем случае в переменную group будут переданы объекты модели Group,
@@ -31,4 +28,3 @@ def group_list(request, slug):
         'posts': posts,
     }
     return render(request, 'posts/group_list.html', context)
-
